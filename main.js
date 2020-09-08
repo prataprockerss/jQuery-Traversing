cb78e2ff92fc4defb2151029a3a09e016ae4da1c
Pratap Rockerss <pratap@techcetra.com>
2020-09-08 14:50:05 +0530

    Intial file changes

---
+++ main.js
@@ -0,0 +1,32 @@
+$(window).on('load',function() {
+    var elem = $('li').not('.intro'); 
+        elem.css('border-style','dotted');
+    console.log(elem);
+})
+
+// all methods for reference 
+
+// // Ancestor
+// parent()
+// parents()
+// parentsUntil()
+
+// // Descendants
+// children()
+// find()
+
+// // Siblings
+// siblings()
+// next()
+// nextAll()
+// nextUntil()
+// prev()
+// prevAll()
+// prevUntil()
+
+// // Filtering
+// first()
+// last()
+// eq()
+// filter()
+// not()
